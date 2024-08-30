01b0bcbe9e794e572cd6af933464786a3ac4d6b0
Mostafa Ahangarha <ahangarha@riseup.net>
2024-08-30 10:31:11 +0330

    Add tailwindcss with some basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import './App.css'
-
 function App() {
 
   return (
-    <>
-      <textarea name="rawText" id="rawText"></textarea>
-      <button type="button">Process</button>
-      <textarea name="invalidSentences" id="invalidSentences"></textarea>
-      <textarea name="validSentences" id="validSentences"></textarea>
-    </>
+    <div className="flex flex-col gap-4 mt-8">
+      <textarea className="border" name="rawText" id="rawText" rows="7" placeholder="Enter your raw text"></textarea>
+      <button className="bg-gray-200" type="button">Process</button>
+      <textarea className="border" name="invalidSentences" id="invalidSentences"></textarea>
+      <textarea className="border" name="validSentences" id="validSentences"></textarea>
+    </div>
   )
 }
 
