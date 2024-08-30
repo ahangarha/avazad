f8f8b73f115d12d436409b9e4072437fd981a237
Mostafa Ahangarha <ahangarha@riseup.net>
2024-08-30 10:19:51 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <textarea name="rawText" id="rawText"></textarea>
+      <button type="button">Process</button>
+      <textarea name="invalidSentences" id="invalidSentences"></textarea>
+      <textarea name="validSentences" id="validSentences"></textarea>
+    </>
+  )
+}
+
+export default App
