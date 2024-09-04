576e57cc37b3367961bfdb71ab3b4aa8fba48976
Mostafa Ahangarha <ahangarha@riseup.net>
2024-09-04 15:07:10 +0330

    Add doesNotExceed15Words validator

--- src/utils/pipelines/validatePipelines.test.js
+++ src/utils/pipelines/validatePipelines.test.js
@@ -1,4 +1,4 @@
-import { doesNotContainsDigits, doesNotContainsSpecialCharacters } from "./validatePipelines";
+import { doesNotContainsDigits, doesNotContainsSpecialCharacters, doesNotExceed15words } from "./validatePipelines";
 
 describe('doesNotContainsDigits', () => {
     it('does not contains digits', () => {
@@ -38,4 +38,29 @@ describe('doesNotContainsSpecialCharacters', () => {
         expect(actual_valids).toEqual(expected_valids)
         expect(actual_invalids).toEqual(expected_invalids)
     });
+})
+
+describe('doesNotExceed15words', () => {
+    it('validates max 15 words in a sentence', () => {
+        const input = [
+            'a b c d e f g h i j k l m n',
+            'a b c d e f g h i j k l m n o',
+            'a b c d e f g h i j k l m n o p'
+        ]
+        const exisitingInvalides = [123]
+
+        const expected_valids = [
+            'a b c d e f g h i j k l m n',
+        ]
+        const expected_invalids = [
+            ...exisitingInvalides,
+            'a b c d e f g h i j k l m n o',
+            'a b c d e f g h i j k l m n o p'
+        ]
+
+        const [actual_valids, actual_invalids] = doesNotExceed15words(input, exisitingInvalides)
+
+        expect(actual_valids).toEqual(expected_valids)
+        expect(actual_invalids).toEqual(expected_invalids)
+    })
 })
