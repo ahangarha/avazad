c437f85e9d6a80818a55c2d58cc88bead3c74218
Mostafa Ahangarha <ahangarha@riseup.net>
2024-09-17 11:01:53 +0330

    Revert "Restores 4d86d981bc1e (last known good state) [hotfix for #6]"
    This reverts commit 70821d15ceb2d821adf701eba9a8dec082206737.

--- src/Batches.jsx
+++ src/Batches.jsx
@@ -1,36 +1,44 @@
 import PropTypes from 'prop-types';
 
+
+const Batch = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  data: PropTypes.array.isRequired,
+  source: PropTypes.string.isRequired,
+  rationale: PropTypes.string.isRequired,
+  domain: PropTypes.string,
+})
+
 function Batches({ batches }) {
   return(
     <div className="flex flex-col gap-4 text-sm text-slate-400 overflow-auto">
-      {batches.map((batch) => (
-        <div key={batch.id} className="flex justify-stretch w-full border border-slate-700 rounded-xl bg-white/5 shadow">
-          <div className="grid place-content-center text-center w-20 p-2 border-e border-e-slate-700">
-            <span>تعداد</span>
-            <span className="text-3xl">{batch.data.length}</span>
-          </div>
-          <div className="flex flex-col flex-grow gap-1 p-2">
-            <p className="overflow-hidden bg-black/10 p-1 rounded w-full">منبع: {batch.source || 'وارد نشده!'}</p>
-            <p className="overflow-hidden bg-black/10 p-1 rounded w-full">پروانه: {batch.rationale || 'وارد نشده!'}</p>
-            <p className="overflow-hidden bg-black/10 p-1 rounded w-full">موضوع: {batch.domain || 'وارد نشده!'}</p>
-          </div>
-        </div>
-      )
-      )}
+      {batches.map((batch) => <SingleBatch batch={batch} key={batch.id} />)}
     </div>
   )
 }
 
 Batches.propTypes = {
-  batches: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      data: PropTypes.array.isRequired,
-      source: PropTypes.string.isRequired,
-      rationale: PropTypes.string.isRequired,
-      doamin: PropTypes.string,
-    })
-  ).isRequired,
+  batches: PropTypes.arrayOf(Batch).isRequired,
 };
 
-export default Batches
+function SingleBatch({ batch }) {
+  return (
+    <div className="flex justify-stretch w-full border border-slate-700 rounded-xl bg-white/5 shadow">
+      <div className="grid place-content-center text-center w-20 p-2 border-e border-e-slate-700">
+        <span>تعداد</span>
+        <span className="text-3xl">{batch.data.length}</span>
+      </div>
+      <div className="flex flex-col flex-grow gap-1 p-2">
+        <p className="overflow-hidden bg-black/10 p-1 rounded w-full">منبع: {batch.source || 'وارد نشده!'}</p>
+        <p className="overflow-hidden bg-black/10 p-1 rounded w-full">پروانه: {batch.rationale || 'وارد نشده!'}</p>
+        <p className="overflow-hidden bg-black/10 p-1 rounded w-full">موضوع: {batch.domain || 'وارد نشده!'}</p>
+      </div>
+    </div>
+  );
+}
+
+SingleBatch.propTypes = {
+  batch: Batch
+};
+
+export default Batches;
